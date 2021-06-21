38de631a7a970c8a168fde806ef7563fe91949a8
Benyw <marciojuniort@gmail.com>
2021-06-20 21:25:01 -0300

    :tada: First commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  return response.send('Fala DEV!');
+});
+
+app.post('/test-post', (request, response) => {
+    return response.send('Suas informações foram captadas com sucesso!');
+})
+
+app.listen(3000, () => {
+  console.log('Server is running!');
+});
