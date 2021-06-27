9e47cb98dc91ab326b54796338f149a20699c6af
Benyw <marciojuniort@gmail.com>
2021-06-27 15:41:00 -0300

    :floppy_disk: 4th class - Landing

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 
-const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 const createTagController = new CreateTagController();
+const createUserController = new CreateUserController();
 
+router.post('/login', authenticateUserController.handle);
 router.post('/users', createUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
+router.post('/compliments', createComplimentController.handle);
 
 export { router };
